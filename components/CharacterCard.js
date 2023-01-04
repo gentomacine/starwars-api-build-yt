ae4ec90a50531e4314228ef132ab4260a2004aa7
gentomacine <ogwaluchidozie@gmail.com>
2023-01-04 18:25:23 +0100

    go

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -1,56 +1,57 @@
+import React from "react";
 import Head from "next/head";
 import styled from "styled-components";
 
 function CharacterCard({ character }) {
-    const locations = [
-        character?.bornLocation,
-        character?.diedLocation,
-        character?.homeworld,
-    ];
+  const locations = [
+    character?.bornLocation,
+    character?.diedLocation,
+    character?.homeworld,
+  ];
 
-    const updateLocations = (value, index, self) => {
-        return self.indexOf(value) === index;
-    };
+  const updateLocations = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
 
-    return (
-        <>
-            <Head>
-                <title>{character?.name} | StarWars.com</title>
-            </Head>
-            <CardContainer>
-                <img src={character?.image} alt="" />
-                <CharacterInfo>
-                    <h1>{character?.name}</h1>
-                    <h4>{character?.affiliations?.length > 0 && "AFFILIATIONS"}</h4>
-                    {character?.affiliations?.map((affiliation, index) => (
-                        <p key={index}>{affiliation}</p>
-                    ))}
-                    <h4>{character?.apprentices?.length > 0 && "APPRENTICES"}</h4>
-                    {character?.apprentices?.map((apprentice, index) => (
-                        <p key={index}>{apprentice}</p>
-                    ))}
-                    <h4>
-                        {(character?.bornLocation && "LOCATIONS") ||
-                            (character?.diedLocation && "LOCATIONS") ||
-                            (character?.homeworld && "LOCATIONS")}
-                    </h4>
-                    {locations.filter(updateLocations).map((location) => (
-                        <p>{location !== "" && location}</p>
-                    ))}
-                    <h4>GENDER</h4>
-                    <span>{character?.gender}</span>
-                    <h4>DIMENSIONS</h4>
-                    <span>Height: {character?.dimensions}m</span>
-                    <h4>SPECIES</h4>
-                    <span>{character?.species}</span>
-                    <h4>WIKI</h4>
-                    <a href={character?.wiki} target="_blank" rel="noreferrer">
-                        Read more about {character?.name}
-                    </a>
-                </CharacterInfo>
-            </CardContainer>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>{character?.name} | StarWars.com</title>
+      </Head>
+      <CardContainer>
+        <img src={character?.image} alt="" />
+        <CharacterInfo>
+          <h1>{character?.name}</h1>
+          <h4>{character?.affiliations?.length > 0 && "AFFILIATIONS"}</h4>
+          {character?.affiliations?.map((affiliation, index) => (
+            <p key={index}>{affiliation}</p>
+          ))}
+          <h4>{character?.apprentices?.length > 0 && "APPRENTICES"}</h4>
+          {character?.apprentices?.map((apprentice, index) => (
+            <p key={index}>{apprentice}</p>
+          ))}
+          <h4>
+            {(character?.bornLocation && "LOCATIONS") ||
+              (character?.diedLocation && "LOCATIONS") ||
+              (character?.homeworld && "LOCATIONS")}
+          </h4>
+          {locations.filter(updateLocations).map((location) => (
+            <p>{location !== "" && location}</p>
+          ))}
+          <h4>GENDER</h4>
+          <span>{character?.gender}</span>
+          <h4>DIMENSIONS</h4>
+          <span>Height: {character?.dimensions}m</span>
+          <h4>SPECIES</h4>
+          <span>{character?.species}</span>
+          <h4>WIKI</h4>
+          <a href={character?.wiki} target="_blank" rel="noreferrer">
+            Read more about {character?.name}
+          </a>
+        </CharacterInfo>
+      </CardContainer>
+    </>
+  );
 }
 
 export default CharacterCard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { createGlobalStyle } from "styled-components";
 import Footer from "../components/Footer";
